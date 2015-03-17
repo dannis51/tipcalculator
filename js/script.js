25003b1fd6a4217d7fe8d9a71a27004b3e69519c
Danni Santana <dannisantana@Dannis-MacBook-Pro.local>
2015-03-16 20:47:02 -0400

    added java

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-//add script here
+function tipCalculate(){
+
+var totalBill = document.getElementById('bill').value;
+
+var percBtns = document.getElementsByClassName('tip-btn');
+
+for(var i=0; i<percBtns.length; i++){
+	if(percBtns[i].checked){
+		var tipPerc = percBtns[i].value;
+	}
+}
+
+var tipTotal = totalBill * tipPerc;
+var tipBox = document.getElementById('tipamount');
+tipBox.innerHTML = '$'+tipTotal;
+
+}
+
