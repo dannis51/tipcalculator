42598fd4bbac0e3131a1b3bfd09ce936c687a507
Danni Santana <dannisantana@Dannis-MacBook-Pro.local>
2015-03-23 19:29:06 -0400

    reset calc

--- js/script.js
+++ js/script.js
@@ -10,9 +10,41 @@ for(var i=0; i<percBtns.length; i++){
 	}
 }
 
-var tipTotal = totalBill * tipPerc;
-var tipBox = document.getElementById('tipamount');
-tipBox.innerHTML = '$'+tipTotal;
+
+
+if(!isNaN(totalBill)){
+	var tipTotal = Math.floor(totalBill * tipPerc * 100) / 100; 
+	var tipBox = document.getElementById('tipamount');
+	tipBox.innerHTML = '$'+tipTotal;
+}
+else {
+	alert('please enter only numbers');
+	buttonReset();
+	
+	}
 
 }
+
+function buttonReset(){
+	document.getElementById('bill').value
+		='';
+
+
+var percBtns = document.getElementsByClassName('tip-btn');
+for(var i=0; i<percBtns.length; i++){
+	percBtns[i].checked = false;
+	}
+
+
+var tipBox = document.getElementById('tipamount');
+	tipBox.innerHTML = '$0.00'
+
+}
+
+
+
+
+
+
+
 
